73a0a68f9666e0bc34c8631a61aacfc62573a215
Loc Tran <loctran@Julies-iMac.local>
2021-06-18 01:11:47 +0700

    render ul todo list

--- src/App.js
+++ src/App.js
@@ -2,23 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let todos = [
+    'Go to market',
+    'Buy food',
+    'Make dinner'
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {todos.map((todo) => (
+        <li>{todo}</li>
+      ))}
+    </ul>
   );
 }
 
